63884eb1aa1e995deca76f873208b9c90ea35cc0
WatchDog-156 <borsukiewiczkuba12345@gmail.com>
2026-03-28 18:36:56 +0100

    Algorytm Branch and Bound

--- inc/problem.h
+++ inc/problem.h
@@ -6,18 +6,21 @@
 
 class Problem {
     int n;
+    int UB=999999;
+    Permutacja best_p = Permutacja(n);
 public:
     Problem(int liczba): n(liczba){}
 
     void setN(int N) {n=N;}
     int getN() {return n;}
     int kryterium(Permutacja p, const std::vector<Zadanie>& dane);
+    void Search_critical_block(Permutacja p, const std::vector<Zadanie>& dane, int critical_parameters[]);    
     int Algorytm_ERD(const std::vector<Zadanie>& dane);
     int Algorytm_EDD(const std::vector<Zadanie>& dane);
     int Algorytm_zupelny(const std::vector<Zadanie>& dane);
     int Algorytm_wlasny(const std::vector<Zadanie>& dane);
-    int Algorytm_Schrage(const std::vector<Zadanie>& dane);
-    // int Algorytm_BandB(const std::vector<Zadanie>& dane);
+    std::pair<int, Permutacja> Algorytm_Schrage(const std::vector<Zadanie>& dane);
+    int Algorytm_BandB(std::vector<Zadanie>& dane);
 };
 
 #endif
--- src/problem.cpp
+++ src/problem.cpp
@@ -121,7 +121,7 @@ int Problem::Algorytm_wlasny(const std::vector<Zadanie>& dane){
     return L;
 }
 
-int Problem::Algorytm_Schrage(const std::vector<Zadanie>& dane){
+std::pair<int, Permutacja> Problem::Algorytm_Schrage(const std::vector<Zadanie>& dane){
     Permutacja p(n);
     std::vector<bool> czy_wykonane(n, false);
     int aktulany_czas = 0;
@@ -161,23 +161,98 @@ int Problem::Algorytm_Schrage(const std::vector<Zadanie>& dane){
     std::cout << "Nalepsze L_max dla algorytmu Schrage " << L << std::endl;
     std::cout << "Permutacja: " << p << std::endl;
 
-    return L;
+    return {L, p};
 }
-// int Problem::Algorytm_BandB(const std::vector<Zadanie>& dane){
-//     Permutacja p(n);
-//     for(int i=0; i<n; i++) p.perm[i]=i;
 
-//     Permutacja best_p = p;
+void Problem::Search_critical_block(Permutacja p, const std::vector<Zadanie>& dane, int critical_parameters[]){
+    int Lmax = -999999;
+    int time = 0;
+    std::vector<int> end_time(this->n);
 
-//     int LB=this->Algorytm_Schrage_z_podzialem(dane);
-//     int UB=this->Algorytm_Schrage(dane);
+    // b
+    for(int i=0; i<this->n; i++){
+        int idx = p.perm[i];
+        time = std::max(time, dane[idx].rj + dane[idx].pj);
+        end_time[i] = time;
+        int L = time - dane[idx].dj;
+        if(L >= Lmax){
+            Lmax = L;
+            critical_parameters[1] = i;
+        }
+    }
 
+    // a
+    critical_parameters[0] = critical_parameters[1];
+    int suma_p = 0;
+    for(int i = critical_parameters[1]; i>=0; i--){
+        suma_p += dane[p.perm[i]].pj;
+        int time_before = 0;
+        if(i==0){
+            time_before = dane[p.perm[i]].rj;
+        } else{
+            time_before = end_time[i-1];
+        }
+        if(end_time[critical_parameters[1]] - time_before == suma_p){
+            critical_parameters[0] = i;
+        }
+    }
 
+    // c
+    critical_parameters[2] = -1;
+    for(int i=critical_parameters[1]-1; i>=critical_parameters[0]; i--){
+        if(dane[p.perm[i]].dj > dane[p.perm[critical_parameters[1]]].dj){
+            critical_parameters[3] = i;
+            break;
+        }
+    }
+}
 
-//     int B=this->Algorytm_Schrage(dane);
-//     if(B<UB){
-//         UB = B;
-//         best_p = p;
-//     }
+int Problem::Algorytm_BandB(std::vector<Zadanie>& dane){
+    std::pair<int, Permutacja> wynik_schrage = this->Algorytm_Schrage(dane);
+    int current_Lmax = wynik_schrage.first;
+    Permutacja current_perm = wynik_schrage.second;
 
-// }
+    if(current_Lmax < this->UB){
+        this->UB = current_Lmax;
+        this->best_p = current_perm;
+    }
+
+    int critical_parameters[3] = {-1,-1,-1};                                    // a = 0, b = 1, c = 2
+    this->Search_critical_block(current_perm, dane, critical_parameters);
+
+    // zadanie jest optymalne w tej gałęzi
+    if(critical_parameters[2]==-1){
+        return this->UB;
+    }
+
+    // parametry dla bloku K
+    int r_star = 999999;
+    int p_star = 0;
+    int d_star = -999999;
+    for(int i=critical_parameters[2]+1; i<=critical_parameters[1]; i++){
+        int idx = current_perm.perm[i];
+        r_star = std::min(r_star, dane[idx].rj);
+        d_star = std::max(d_star, dane[idx].dj);
+        p_star += dane[idx].pj;
+    }
+
+    int idx_c = current_perm.perm[critical_parameters[2]];
+
+    // lewa gałąź, c przed blokiem K
+    int old_dj = dane[idx_c].dj;
+    dane[idx_c].dj = std::min(dane[idx_c].dj, d_star - p_star);
+    if(this->Algorytm_Schrage_z_podzialem(dane) < this->UB){
+        this->Algorytm_BandB(dane);
+    }
+    dane[idx_c].dj = old_dj;
+
+    // prawa gałąź, c po bloku K
+    int old_rj = dane[idx_c].rj;
+    dane[idx_c].rj = std::max(dane[idx_c].rj, r_star + p_star);
+    if(this->Algorytm_Schrage_z_podzialem(dane) < this->UB){
+        this->Algorytm_BandB(dane);
+    }
+    dane[idx_c].rj = old_rj;
+
+    return this->UB;
+}
